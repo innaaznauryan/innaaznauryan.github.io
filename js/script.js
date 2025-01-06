27ad0b316e03e57d44d682591fe01145d28bc702
innaaznauryan <inna.aznauryan@gmail.com>
2025-01-07 01:16:29 +0400

    style updates

--- js/script.js
+++ js/script.js
@@ -1,3 +1,12 @@
-const element = document.getElementById("year");
+const copyrightYear = document.getElementById("copyright-year");
 const year = new Date().getFullYear();
-element.textContent = year;
+copyrightYear.textContent = year;
+
+const localTime = document.getElementById("local-time");
+const updateTime = () => {
+    const now = new Date();
+    const timeString = now.toLocaleTimeString('en-US', { hour: '2-digit', minute: '2-digit' });
+    localTime.innerText = `Local Time: GMT+4 - ${timeString}`;
+};
+updateTime();
+setInterval(updateTime, 1000);
